5587396bdb038ef8381a2d21dfb4839f785920f5
Jordan Holm <Jordanholm12@gmail.com>
2024-02-27 20:28:13 -0700

    Project created, player class started

---
+++ casinoMenu.py
@@ -0,0 +1,8 @@
+from player import Player
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ player.py
@@ -0,0 +1,18 @@
+class Player():
+    def __init__(self, name, money):
+        self.name = name
+        self.money = float(money)
+
+    def adjust_money(self, amount_to_adjust):
+        '''Adjust class objects money variable by the given "amount_to_adjust" variable when called'''
+        self.money += amount_to_adjust
+
+    def show_money(self):
+        '''Displays the class object current money amount. Formated to 2 decimal places'''
+        return f"${self.money:.2f}"
+
+    def __str__(self):
+        return f"{self.name}: {self.show_money()}"
+
+    def __repr__(self):
+        return f"Player({self.name}, {self.money})\nPlayer.adjust_money(-10)\nPlayer.show_money()"
