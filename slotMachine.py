230cffd1cc8e37b1b9eba98eb2751bbe8b8709ee
Jordan Holm <Jordanholm12@gmail.com>
2024-03-27 18:33:25 -0600

    comments of old game_win() deleted

--- slotMachine.py
+++ slotMachine.py
@@ -12,23 +12,17 @@ def check_slot_nums(player:Player, bet:float, num1:int, num2:int, num3:int):
     if num1 == num2:
         if num2 == num3:
             if num1 == 1: #if the player rolls three 1s 2x multiplier
-                # game_win(player, bet)
                 player.player_win(bet, 2)
             elif num1 == 2: #if the player rolls three 2s 4x multiplier
-                # game_win(player, bet, 4)
                 player.player_win(bet, 4)
             elif num1 == 3: #if the player rolls three 3s 10x multiplier
                 print("JACKPOT!")
-                # game_win(player, bet, 10)
                 player.player_win(bet, 10)
         else:
-            # game_tie(player, bet)
             player.player_tie(bet)
     elif num2 == num3:
-        # game_tie(player, bet)
         player.player_tie(bet)
     else:
-        # game_lose(player, bet)
         player.player_lose(bet)
 
 def play_game(player:Player):
