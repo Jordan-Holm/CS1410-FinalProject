77aa5d01310eca8a4e721160ff92ad7a67409356
prtrjrdn02 <161663177+prtrjrdn02@users.noreply.github.com>
2024-04-01 16:00:41 -0600

    I Made Cointoss
    Cointoss has been programmed in a very general way where it asks for heads or tails and then randomly chooses heads or tails and if your guess matches what the program chose then you win the bet money. If not you lose the bet money.
    
    How it is implemented into the other programs is a mystery to me so go ahead and look at it and see if there is anything that needs to be changed in order to be used in the program as a whole.

--- coinFlip.py
+++ coinFlip.py
@@ -1,5 +1,52 @@
 from player import Player
+import random
 
+class Coin():
+    def __init__(self, coin, guess, bet):
+        self.coin = coin
+        self.guess = guess
+        self.bet = bet
+        
+    def cointoss(self):
+        def sign():
+            if self.coin == 1:
+                self.coin = "Heads"
+            elif self.coin == 2:
+                self.coin = "Tails"
+        def game_lose(player:Player, bet:float):
+            player.adjust_money(-bet)
+            print(f"It was {self.coin}!You lost ${-bet}.")
 
-def play_game(player:Player):
-    pass
+        def game_win(player:Player, bet:float):
+            player.adjust_money(bet)
+            print(f"It was {self.coin}! You won ${bet}!")
+            
+        def game_results():
+            if self.guess == self.coin:
+                sign()
+                game_win()
+            
+            elif self.guess != self.coin:
+                sign()
+                game_lose()
+            
+def main():
+    player.set_cur_bet()
+    bet = player.get_cur_bet()
+    
+    guess = int(input('1.Heads\n2.Tails\nHeads or Tails? Type a number. ' ))
+    try:
+        guess == 1 or 2 
+    except ValueError as e:
+        print(e)
+        
+    coin = Coin(random.randint(1,2), guess, bet)
+    coin.cointoss()
+      
+    
+    
+if __name__ == "__main__":
+    main()
+        
+            
+        
